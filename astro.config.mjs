dbd549f14c9ad85c2477a78a0ddf9d8b2f59d8ee
mitrabawangid <mitrabawang.id@gmail.com>
2026-04-12 06:09:29 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,7 +35,7 @@ function serializeSitemapItem(item) {
 
 	if (slug === '/') {
 		priority = 1;
-	} else if (slug === '/info-harga-bawang-merah') {
+	} else if (slug === '/harga-bawang-merah-hari-ini') {
 		priority = 0.95;
 	} else if (slug === '/about' || slug === '/exporter-red-onion-shallot-from-indonesia') {
 		priority = 0.85;
